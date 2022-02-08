c9322ac0f23950fcb657127c7a9686e68f7d637e
TayyabaZubair <74321242+TayyabaZubair@users.noreply.github.com>
2022-02-08 18:14:02 +0500

    Add files via upload

---
+++ E-commerce Store/Frontend/src/reducers/productReducers.js
@@ -0,0 +1,72 @@
+// importing productConstants
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
+  PRODUCT_DETAIL_FAIL,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAIL,
+  PRODUCT_CREATE_REVIEW_RESET,
+} from "../constants/productConstants";
+
+export const productListReducer = (
+  state = { products: [], count: 0 },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] };
+    case PRODUCT_LIST_SUCCESS:
+      return {
+        loading: false,
+        products: action.payload.products,
+        count: action.payload.count,
+      };
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAIL_REQUEST:
+      return { loading: true, ...state };
+    case PRODUCT_DETAIL_SUCCESS:
+      // console.log(action.payload.reviews);
+      return {
+        loading: false,
+        product: action.payload.product,
+        reviews: action.payload.reviews,
+      };
+    case PRODUCT_DETAIL_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productReviewCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REVIEW_REQUEST:
+      return { loading: true };
+    case PRODUCT_CREATE_REVIEW_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      };
+    case PRODUCT_CREATE_REVIEW_FAIL:
+      return { loading: false, error: action.payload };
+    case PRODUCT_CREATE_REVIEW_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
